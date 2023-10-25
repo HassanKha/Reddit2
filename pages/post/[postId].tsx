910162b058c758e2fcd233b1932fdd409db2b78a
HassanKha <hassankhaled4432@gmail.com>
2023-10-25 15:03:11 +0300

    done

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -95,8 +95,8 @@ function PostPage() {
       </div>
       <div className="-my-5 rounded-b-md border-t-0 border-gray-300 bg-white py-5 px-10">
         <hr className="py-2" />
-        {post?.comments.map((comment) => (
-          <div className="relative flex items-center space-x-2 space-y-5" key={comment.id}>
+        {post?.comments.map((comment,i) => (
+          <div className="relative flex items-center space-x-2 space-y-5" key={i}>
             <hr className='absolute z-0 top-10 h-16 border left-7' />
             <div className='z-50' >
               <Avatar />
